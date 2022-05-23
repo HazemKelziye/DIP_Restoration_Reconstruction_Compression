ea1b64e3f535cb91c0d0ee4df592118df00450f8
UbaydaAbdulsamad <ubaydasmd@gmail.com>
2022-05-23 21:45:32 +0300

    Noises are done

--- APP.py
+++ APP.py
@@ -5,7 +5,7 @@ image = cv.imread("Nature.jpg")
 image = cv.resize(image, (800, 600), interpolation=cv.INTER_LINEAR)
 cv.imshow('image', image)
 
-noisy = gaussian(image)
+noisy = noise_uniform(image, 1, 100)
 noisy = cv.resize(noisy, (800, 600), interpolation=cv.INTER_LINEAR)
 cv.imshow('noisy', noisy)
 
--- filters.py
+++ filters.py
@@ -1,111 +1,60 @@
-# import cv2
-# import os
-# import matplotlib.pyplot as plt
 import numpy as np
 import cv2 as cv
-import skimage
+import random
 
-def gaussian(image):
+
+def noise_gaussian(image, mean, var):
     row, col, ch = image.shape
-    mean = 0.2
-    var = 10
     sigma = var ** 0.5
     gauss = np.random.normal(mean, sigma, (row, col, ch))
-    gauss = gauss.reshape(row, col, ch)
     gauss = np.array(gauss, dtype=np.uint8)
     noisy = cv.add(image, gauss)
     return noisy
 
-def sp_noise(image, prob):
-    '''
-    Add salt and pepper noise to image
-    prob: Probability of the noise
-    '''
-    output = image.copy()
-    if len(image.shape) == 2:
-        black = 0
-        white = 255
-    else:
-        colorspace = image.shape[2]
-        if colorspace == 3:  # RGB
-            black = np.array([0, 0, 0], dtype='uint8')
-            white = np.array([255, 255, 255], dtype='uint8')
-        else:  # RGBA
-            black = np.array([0, 0, 0, 255], dtype='uint8')
-            white = np.array([255, 255, 255, 255], dtype='uint8')
-    probs = np.random.random(image.shape[:2])
-    image[probs < (prob / 2)] = black
-    image[probs > 1 - (prob / 2)] = white
+def noise_salt_pepper(image, prob):
+
+    threshold = 1 - prob
+
+    for i in range(0, image.shape[0]):
+        for j in range(0, image.shape[1]):
+
+            # return a value between [0, 1]
+            rdn = random.random()
+
+            if rdn < prob:
+                image[i][j] = 0
+
+            elif rdn > threshold:
+                image[i][j] = 255
+
+            else:
+                image[i][j] = image[i][j]
     return image
 
-# def salt_pepper(image):
-#     prob = 0.01
-#     threshold = 1 - prob
-#     for i in range(0, image.shape[0]):
-#         for j in range(0, image.shape[1]):
-#             rdn = random.random()
-#             if rdn < prob:
-#                 image[i][j] = 0
-#             elif rdn > thres:
-#                 image[i][j] = 255
-#             else:
-#                 image[i][j] = image[i][j]
-#     plt.hist(image, bins='auto')
-#     plt.show()
-#     return image
-#
-#     elif noise_type == "poisson".lower():
-#     vals = len(np.unique(image))
-#     vals = 2 ** np.ceil(np.log2(vals))
-#     noisy = np.random.poisson(image * vals) / float(vals)
-#     noisy = np.array(noisy, dtype=np.uint8)
-#     plt.hist(noisy, bins=5)
-#     plt.show()
-#     return noisy
-#
-# elif noise_type == "speckle".lower():
-# row, col = image.shape
-# mean = 0
-# var = 0.1
-# gauss = np.random.normal(mean, math.sqrt(var), (row, col))
-# noisy = image + image * gauss
-# noisy = np.array(noisy, dtype=np.uint8)
-# # plt.hist(noisy,bins='auto')
-# # plt.show()
-# return noisy
-# elif noise_type == "rayleigh".lower():
-# row, col = image.shape
-# mean = 0
-# var = 0.1
-# sigma = var ** 0.5
-# s = stats.mode(image, axis=None)
-# print("s=", s[0])
-# s = s[0]
-# ray = np.random.rayleigh(scale=10, size=(row, col))
-# # print(gauss.shape)
-# # plt.hist(ray, bins='auto')
-# # plt.show()
-# ray = ray.reshape(row, col)
-# noisy = image + ray
-# noisy = np.array(noisy, dtype=np.uint8)
-# return noisy
-# elif noise_type == "Gamma".lower():
-# row, col = image.shape
-# gamma = np.random.gamma(shape=2, scale=10, size=(row, col))
-# plt.hist(gamma, bins='auto')
-# plt.show()
-# image = image + gamma
-# image = np.array(image, dtype=np.uint8)
-# # plt.hist(image, bins='auto')
-# # plt.show()
-# return image
-# elif noise_type == "Exponential".lower():
-# row, col = image.shape
-# exponen = np.random.exponential(scale=3, size=(row, col))
-# plt.hist(exponen, bins='auto')
-# plt.show()
-# image = image + exponen
-# image = np.array(image, dtype=np.uint8)
-# plt.hist(image, bins='auto')
-# plt.show()
-# return image
+def noise_rayleigh(image, mean):
+    row, col, ch = image.shape
+    ray = np.random.rayleigh(scale=mean, size=(row, col, ch))
+    ray = np.array(ray, dtype=np.uint8)
+    noisy = cv.add(image, ray)
+    return noisy
+
+def noise_gamma(image, mean, variance):
+    row, col, ch = image.shape
+    gamma = np.random.gamma(shape=mean, scale=variance, size=(row, col, ch))
+    gamma = np.array(gamma, dtype=np.uint8)
+    image = cv.add(image, gamma)
+    return image
+
+def noise_exponential(image, mean):
+    row, col, ch = image.shape
+    expo = np.random.exponential(scale=mean, size=(row, col, ch))
+    expo = np.array(expo, dtype=np.uint8)
+    image = cv.add(image, expo)
+    return image
+
+def noise_uniform(image, a, b):
+    row, col, ch = image.shape
+    uni = np.random.uniform(low=a, high=b, size=(row, col, ch))
+    uni = np.array(uni, dtype=np.uint8)
+    image = cv.add(image, uni)
+    return image
